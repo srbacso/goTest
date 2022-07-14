ca2aaf29692dbc3f924bd23ae25f75f262360fee
Stephen Bacso <stephen.bacso@gmail.com>
2022-07-14 11:03:08 -0400

    add colors

---
+++ map/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Start")
+
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#00ff00",
+		"blue":  "#0000ff",
+	}
+
+	fmt.Println(colors)
+}
